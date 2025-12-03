d3e324a430632132e325ebd995f55a8bf0c31e3f
Matt Bolt <mbolt35@gmail.com>
2025-12-02 21:57:33 -0500

    Drop exclusive reference requirement for dump() (#26)

--- src/connection.rs
+++ src/connection.rs
@@ -32,7 +32,7 @@ impl Conntrack {
 
     /// The dump call will list all connection tracking for the `Conntrack` table as a
     /// `Vec<Flow>` instances.
-    pub fn dump(&mut self) -> Result<Vec<Flow>> {
+    pub fn dump(&self) -> Result<Vec<Flow>> {
         let genlhdr = GenlmsghdrBuilder::default()
             .cmd(0u8)
             .version(libc::NFNETLINK_V0 as u8)
